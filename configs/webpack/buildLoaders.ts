8b1c05b8e650a102846f8b03d1db8205de7dfe00
enein2860 <dondigidon.nemets@mail.ru>
2026-02-25 19:02:41 +0300

    added bundle analyzer

---
+++ configs/webpack/buildLoaders.ts
@@ -0,0 +1,49 @@
+import webpack from 'webpack'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import { BuildOptions } from './types/config'
+
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif|woff2|woff|TTF)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    }
+
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    }
+
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+                loader: 'css-loader',
+                options: {
+                    esModule: false,
+                    modules: {
+                        auto: (resPath: string) =>
+                            Boolean(resPath.includes('.module.')),
+                        localIdentName: isDev
+                            ? '[path][name]__[local]'
+                            : '[hash:base64:8]',
+                        namedExport: false,
+                    },
+                },
+            },
+            'sass-loader',
+        ],
+    }
+
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
+
+    return [typescriptLoader, cssLoader, svgLoader, fileLoader]
+}
